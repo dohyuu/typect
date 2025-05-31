131d74ae80648ea3eb09539cb529c62616589e77
dodohyuns <hdhcoss@gmail.com>
2025-06-01 04:11:32 +0900

    feat: add typedArray for casting

--- src/primitives/array/array.ts
+++ src/primitives/array/array.ts
@@ -1,2 +1,35 @@
+import type { CompareLength } from "./compare"
+
 export type StringToArray<S extends string> =
   S extends `${infer Head}${infer Rest}` ? [Head, ...StringToArray<Rest>] : []
+
+export type TypedArray<
+  A extends unknown[],
+  B extends unknown[],
+> = A extends unknown[]
+  ? CompareLength<A, B> extends 1
+    ? TypedArrayShortInput<A, B>
+    : TypedArrayLongInput<A, B>
+  : never
+
+export type TypedArrayLongInput<
+  A extends unknown[],
+  Types extends unknown[],
+> = {
+  [K in keyof A]: K extends keyof Types
+    ? A[K] extends Types[K]
+      ? A[K]
+      : never
+    : never
+}
+
+export type TypedArrayShortInput<
+  A extends unknown[],
+  Types extends unknown[],
+> = {
+  [K in keyof Types]: K extends keyof A
+    ? A[K] extends Types[K]
+      ? A[K]
+      : never
+    : never
+}
