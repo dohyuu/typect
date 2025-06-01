b619b362d845e4bc6bb624fcdac24a57d1dee56e
dodohyuns <hdhcoss@gmail.com>
2025-06-02 04:43:45 +0900

    feat: add ComposeUnsafe

--- src/hkt/compose.ts
+++ src/hkt/compose.ts
@@ -8,3 +8,10 @@ export interface Compose<
 > extends Lambda<Params<F0["sig"]>, SigReturnOf<F1, [SigReturnOf<F0>]>> {
   readonly return: CallArg1<F1, CallArg1<F0, UnsafeArgs0Of<this>>>
 }
+
+export interface ComposeUnsafe<
+  F1 extends Lambda<[SigReturnOf<F0>], unknown>,
+  F0 extends Lambda<never, unknown>,
+> extends Lambda<Params<F0["sig"]>, SigReturnOf<F1, [SigReturnOf<F0>]>> {
+  readonly return: CallArg1<F1, CallArg1<F0, UnsafeArgs0Of<this>>>
+}
