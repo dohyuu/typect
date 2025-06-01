f020355bc086f36e90c5b528ff32b4a5778d0772
dodohyuns <hdhcoss@gmail.com>
2025-06-02 03:58:53 +0900

    fix: wrong import

--- src/expect.ts
+++ src/expect.ts
@@ -1,22 +1,23 @@
-import type { _ToContain } from "./expectors/contain"
-import type { _ToBeDefined } from "./expectors/defined"
-import type { _ToEqual } from "./expectors/equal"
-import type { _ToBeFalsy } from "./expectors/falsy"
-import type { _ToBeGreaterThan } from "./expectors/greater-than"
-import type { _ToBeGreaterThanOrEqual } from "./expectors/greater-than-or-equal"
-import type { _ToBeInstanceOf } from "./expectors/instance-of"
-import type { _ToHaveLength } from "./expectors/length"
-import type { _ToBeLessThan } from "./expectors/less-than"
-import type { _ToBeLessThanOrEqual } from "./expectors/less-than-or-equal"
-import type { _ToMatch } from "./expectors/match"
-import type { _ToBeNull } from "./expectors/null"
-import type { _ToBeOneOf } from "./expectors/one-of"
-import type { _ToHaveProperty } from "./expectors/property"
-import type { Reverse } from "./expectors/reverse"
-import type { _ToBeTruthy } from "./expectors/truthy"
-import type { _ToBeUndefined } from "./expectors/undefined"
+import type { _Returns } from "./helpers/returns"
+import type { Reverse } from "./helpers/reverse"
 import type { Lambda } from "./hkt/lambda"
 import type { Match } from "./match"
+import type { _ToContain } from "./matchers/contain"
+import type { _ToBeDefined } from "./matchers/defined"
+import type { _ToEqual } from "./matchers/equal"
+import type { _ToBeFalsy } from "./matchers/falsy"
+import type { _ToBeGreaterThan } from "./matchers/greater-than"
+import type { _ToBeGreaterThanOrEqual } from "./matchers/greater-than-or-equal"
+import type { _ToBeInstanceOf } from "./matchers/instance-of"
+import type { _ToHaveLength } from "./matchers/length"
+import type { _ToBeLessThan } from "./matchers/less-than"
+import type { _ToBeLessThanOrEqual } from "./matchers/less-than-or-equal"
+import type { _ToMatch } from "./matchers/match"
+import type { _ToBeNull } from "./matchers/null"
+import type { _ToBeOneOf } from "./matchers/one-of"
+import type { _ToHaveProperty } from "./matchers/property"
+import type { _ToBeTruthy } from "./matchers/truthy"
+import type { _ToBeUndefined } from "./matchers/undefined"
 import type { IsNever } from "./predicate/is-never"
 import type { Class } from "./primitives/class"
 
@@ -122,5 +123,27 @@ export namespace Not {
     export type HaveProperty<K extends string> = Reverse<_ToHaveProperty<K>>
     export type Match<A extends object> = Reverse<_ToMatch<A>>
     export type BeOneOf<A extends unknown[]> = Reverse<_ToBeOneOf<A>>
+  }
+}
+
+// biome-ignore format: ""
+export namespace Returns {
+  export namespace Not {
+    export type ToEqual<A> = Reverse<_Returns<_ToEqual<A>>>
+    export type ToBeInstanceOf<A extends Class> = Reverse<_Returns<_ToBeInstanceOf<A>>>
+    export type ToBeDefined = Reverse<_ToBeDefined>
+    export type ToBeUndefined = Reverse<_ToBeUndefined>
+    export type ToBeNull = Reverse<_ToBeNull>
+    export type ToBeFalsy = Reverse<_ToBeFalsy>
+    export type ToBeTruthy = Reverse<_ToBeTruthy>
+    export type ToBeLessThan<A extends number> = Reverse<_ToBeLessThan<A>>
+    export type ToBeLessThanOrEqual<A extends number> = Reverse<_ToBeLessThanOrEqual<A>>
+    export type ToBeGreaterThan<A extends number> = Reverse<_ToBeGreaterThan<A>>
+    export type ToBeGreaterThanOrEqual<A extends number> = Reverse<_ToBeGreaterThanOrEqual<A>>
+    export type ToContain<A extends unknown[]> = Reverse<_ToContain<A>>
+    export type ToHaveLength<A extends number> = Reverse<_ToHaveLength<A>>
+    export type ToHaveProperty<K extends string> = Reverse<_ToHaveProperty<K>>
+    export type ToMatch<A extends object> = Reverse<_ToMatch<A>>
+    export type ToBeOneOf<A extends unknown[]> = Reverse<_ToBeOneOf<A>>
   }
 }
